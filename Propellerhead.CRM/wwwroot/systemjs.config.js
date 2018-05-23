93586eb0675bd6d149a6aeb5ae4e14d3eb8db525
alexgflegel <alex.g.flegel@gmail.com>
2018-05-23 18:02:19 +0900

    Corrected the rxjs issue.  Default extension was not configured.

--- Propellerhead.CRM/wwwroot/systemjs.config.js
+++ Propellerhead.CRM/wwwroot/systemjs.config.js
@@ -28,13 +28,16 @@
 
 			// other libraries
 			"rxjs": "npm:rxjs",
-			"tslib": "npm:tslib/tslib.js",
+			"tslib": "npm:tslib/tslib.js"
 
 		},
 		// packages tells the System loader how to load when no filename and/or no extension
 		packages: {
 			app: {
 				main: "./main.js",
+				defaultExtension: "js"
+			},
+			rxjs: {
 				defaultExtension: "js"
 			}
 		}

6a8d9249e87f4a3deeb9a8a5d11703d0c4f5d9d5
alexgflegel <alex.g.flegel@gmail.com>
2018-05-27 22:04:48 +0900

    disabled the non id route for the edit customer component to avoid any unexpected errors.

--- Propellerhead.CRM/wwwroot/app/Routes.ts
+++ Propellerhead.CRM/wwwroot/app/Routes.ts
@@ -6,7 +6,7 @@ import { PortraitComponent } from "./Home/PortraitComponent";
 
 const appRoutes: Routes = [
 	{ path: "", pathMatch: "full" , component: HomeComponent, data: { title: "Home" } },
-	{ path: "customer/", component: EditComponent, data: { title: "Add Customer" } },
+	//{ path: "customer/", component: EditComponent, data: { title: "Add Customer" } },
 	{ path: "customer/:id", component: EditComponent, data: { title: "Edit Customer" } },
 	{ path: "portrait", component: PortraitComponent, data: { title: "Portrait" } }
 ];
